cd3125b0ff841b9a39a7372134436983777c4ab7
wqtsoft <wqysoft@163.com>
2019-09-04 19:48:11 +0800

    add hover+part.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-var reduce=$('.detail .number .reduce');
-reduce.on('click',function(){
-    var num=$('.detail .number .num').text();
-         num++;
-})
+$(document).ready(function(){
+    var num=$(".num");
+    var cart=$("nav ul li span:last-child");
+    // +操作
+    $(".plus").click(function() {
+        num.val(parseInt(num.val())+1);
+        })
+    // -操作
+    $(".reduce").click(function () {
+        num.val(parseInt(num.val())-1);
+        if (parseInt(num.val()) < 0) num.val(0);
+    // 加入购物车
+    $(".detail_right_rec .btn button").click(function(){
+        cart.text(parseInt(cart.text())+1);
+            })
+        })
+    $("#SalesVolume .clearFloat div").mouseenter(function () {
+        $("#SalesVolume .clearFloat img").css("border","2px solid #b09169");
+        $("#SalesVolume .clearFloat p").css("color","#b09169");
+
+    })
+    })
