c3728e75c373e6fce6425f607ec7bfeff7e4ab6f
wqtsoft <wqysoft@163.com>
2019-11-13 11:18:06 +0800

    改用vue布局

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     var num=$(".num");
     var cart=$("nav ul li span:last-child");
-    // +操作
+    // + 操作
     $(".plus").click(function() {
         num.val(parseInt(num.val())+1);
         })
-    // -操作
+    // -  操作
     $(".reduce").click(function () {
         num.val(parseInt(num.val())-1);
         if (parseInt(num.val()) < 0) num.val(0);
@@ -14,9 +14,6 @@ $(document).ready(function(){
         cart.text(parseInt(cart.text())+1);
             })
         })
-    $("#SalesVolume .clearFloat div").mouseenter(function () {
-        $("#SalesVolume .clearFloat img").css("border","2px solid #b09169");
-        $("#SalesVolume .clearFloat p").css("color","#b09169");
 
-    })
+
     })
